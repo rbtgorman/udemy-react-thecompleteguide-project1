cc81e918838efe533dad0528274c6691af3e1275
Robert Gorman <rbtgorman@gmail.com>
2024-06-03 14:28:07 -0400

    Intial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/investment-calculator-logo.png';
+
+export default function Header()  {
+    return (
+    <header id="header">
+        <img src={logo} alt="Logo showing a moneybag" />
+        <h1>Investment Calculator</h1>
+    </header>
+    );
+
+}
