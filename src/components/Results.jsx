a6be341c2bd3f288aaebd3ec84991a65233b448e
Robert Gorman <rbtgorman@gmail.com>
2024-06-05 11:32:40 -0400

    Updated results page

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,6 +2,12 @@ import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
 export default function Results({ input }) {
     const resultsData = calculateInvestmentResults(input);
+
+    // Check if the duration is 0 or negative
+    if (input.duration <= 0) {
+        return <p className="center">Invalid input</p>;
+    }
+
     const initialInvestment =
         resultsData[0].valueEndOfYear -
         resultsData[0].interest -
